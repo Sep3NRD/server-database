8856c0068d3ccdeda874f7a0db8306c79f6dd025
duart23 <331347@via.dk>
2023-12-03 12:28:52 +0100

    comments

--- server-database/src/main/java/Sep3/serverdatabase/service/Implementations/ItemServiceImpl.java
+++ server-database/src/main/java/Sep3/serverdatabase/service/Implementations/ItemServiceImpl.java
@@ -30,6 +30,7 @@ public class ItemServiceImpl extends ItemServiceGrpc.ItemServiceImplBase {
     }
 
 
+    //This method creates an Item and stores it in the database
     @Override
     public void postItem(ItemP request, StreamObserver<ItemP> responseObserver ){
 
@@ -61,13 +62,17 @@ public class ItemServiceImpl extends ItemServiceGrpc.ItemServiceImplBase {
 
     }
 
+    //This method gets All the Items from the database
     @Override
     public void getItems(Empty request, StreamObserver<ItemP> responseObserver){
 
        try{
+           // Log the start of the method execution
            LOG.info(">>> executing getProduct()..");
+           // Retrieve all items from the repository
            List<Item> items = repository.findAll();
 
+           // Iterate through each item and convert it to the gRPC response type
            for (Item item : items) {
                ItemP itemP = ItemP.newBuilder()
                        .setCategory(item.getCategory())
@@ -77,26 +82,34 @@ public class ItemServiceImpl extends ItemServiceGrpc.ItemServiceImplBase {
                        .setName(item.getName())
                        .setItemId(item.getId())
                        .build();
-
+               // Log the items retrieved from the database
                responseObserver.onNext(itemP);
            }
-
+           // Log the items retrieved from the database
            System.out.println("Items from database>>>>>>>"+items.toString());
+           // Notify the client that the operation is completed
            responseObserver.onCompleted();
        }
        catch (Exception e){
+           // Log the error message and stack trace
            LOG.error("Error while fetching items from the repository", e);
+           // Notify the client about the error and provide a generic error message
            responseObserver.onError (new StatusRuntimeException(Status.INTERNAL.withDescription("Could not get items from the repository")));
        }
     }
 
+    //This method gets a specific Item through its id
     @Override
     public void getItemById(GetItemById request, StreamObserver<ItemResponseP> responseObserver){
+        // Extract the item ID from the gRPC request
         int id = request.getItemId();
 
         try{
+            // Attempt to find the item in the repository by ID
             Optional<Item> optionalItem = repository.findById(id);
+
             if (optionalItem.isPresent()){
+                // If the item is found, convert it to the gRPC response type
                 Item item = optionalItem.get();
 
                 ItemP itemP = ItemP.newBuilder()
@@ -108,64 +121,84 @@ public class ItemServiceImpl extends ItemServiceGrpc.ItemServiceImplBase {
                         .setStock(item.getStock())
                         .build();
 
+                // Build the response containing the converted item and send it to the client
                 ItemResponseP itemResponseP = ItemResponseP.newBuilder().setItem(itemP).build();
                 responseObserver.onNext(itemResponseP);
                 responseObserver.onCompleted();
             }
             else {
+                // If the item is not found, notify the client with a NOT_FOUND status and a descriptive error message
                 String errorMessage = "Item with id " + id + " not found";
                 responseObserver.onError(Status.NOT_FOUND.withDescription(errorMessage).asException());
             }
         }
 
         catch (Exception e){
+            // Handle exceptions that may occur during item retrieval
             responseObserver.onError(e);
         }
     }
 
+    //This method updates the parameters "Price" and "Stock" of an Item
     @Override
     public void updateItem(UpdateItemRequest request, StreamObserver<StringValue> responseObserver ){
+        // Extract the item ID from the gRPC request
         int id = request.getItemId();
 
         try {
+            // Attempt to find the item in the repository by ID
             Optional<Item> optionalItem = repository.findById(id);
             if (optionalItem.isPresent()) {
+                // If the item is found, update its properties with the values from the gRPC request
                 Item item = optionalItem.get();
-
                         item.setPrice(request.getPrice());
                         item.setStock(request.getStock());
 
+                // Save the updated item to the repository
                 repository.save(item);
 
+                // Notify the client that the item has been successfully updated
                 responseObserver.onNext(StringValue.newBuilder().setValue("Item Updated").build());
                 responseObserver.onCompleted();
             }
             else {
+                // If the item is not found, notify the client with a NOT_FOUND status and a descriptive error message
                 String errorMessage = "Item with id "+ id +" was not found";
                 responseObserver.onError(Status.NOT_FOUND.withDescription(errorMessage).asException());
             }
         }
         catch (Exception e){
+            // Handle exceptions that may occur during item update
             responseObserver.onError(e);
         }
     }
 
+    //This method deletes an item from the database
     @Override
     public void deleteItemById(DeleteItemRequest request, StreamObserver<Empty> responseObserver) {
         try {
+            // Extract the item ID from the gRPC request
             int itemId = request.getItemId();
+            // Log the item ID (optional, for debugging purposes)
             System.out.println(itemId);
 
+            // Attempt to find the item in the repository by ID
             Optional<Item> optionalItem = repository.findById(itemId);
+
             if (optionalItem.isPresent()) {
+                // If the item is found, delete it from the repository
                 repository.deleteById(itemId);
+
+                // Notify the client that the item has been successfully deleted
                 responseObserver.onNext(Empty.getDefaultInstance());
                 responseObserver.onCompleted();
             } else {
+                // If the item is not found, notify the client with a NOT_FOUND status and a descriptive error message
                 responseObserver.onError(new StatusRuntimeException(
                         Status.NOT_FOUND.withDescription("Item with ID " + itemId + " not found")));
             }
         } catch (Exception e) {
+            // Notify the client about the error and provide a generic error message
             e.printStackTrace();
             responseObserver.onError(new Throwable("Could not delete item from the database"));
         }
